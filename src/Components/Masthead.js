668389090cbad2bc6f37627b23e3b2e970960d8c
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2022-04-20 02:52:10 -0400

    css changes, hd image for masthead

--- src/Components/Masthead.js
+++ src/Components/Masthead.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import header from "../images/header.jpg";
+import bg2 from "../images/bg2.jpg";
 
 function masthead() {
     
     return (
         <div className="masthead" style={{
-            backgroundImage: `url(${header})`,
+            backgroundImage: `url(${bg2})`,
             backgroundSize: 'cover',
             overflow: 'hidden',
           }}>
    
             <div className="App-header">
-                <h1 className="welcome-text">BDS Quality Services</h1>
-                    <p>Your ISO9000/AS9100 Internal Assessment Provider</p>
+                <h1 className="welcome-text"> BDS Quality Services </h1>
+                    <h5> Your ISO9000/AS9100 Internal Assessment Provider </h5>
             </div>
         </div>
   
